e1fa3ba8805b5ed2ce1aad47446076d6fd9807f6
jaydennnn2003 <s4012116@student.rmit.edu.au>
2024-04-16 12:19:13 +1000

    started javascript practice

---
+++ week 5/script.js
@@ -0,0 +1,47 @@
+// console.log("hello world again");
+// let name = "jayden";
+// console.log("my name is", name);
+
+// // thi in s a comment
+// /* this is a
+// mulitliine comment */
+
+// // let city = prompt("where do you live");
+// connsole.log(city);
+// console.log(name);
+
+// const assignment1 = "20";
+// const assignment2 = "20";
+// const assignment3 = "30";
+
+// let total =
+//   parseInt(assignment1) + parseInt(assignment2) + parseInt(assignment3);
+// console.log("mytotal", total);
+
+// let total2 = add();
+// function add() {
+//   let a = 30;
+//   console.log(a);
+// }
+
+// let temperature = prompt("enter todays temperature");
+
+
+const body = document.querySelector("body");
+function checkWeather {
+let temperature = document.querySelector("#temperature");
+if (temperature < 20 && termperature >= 10) {
+  console.log("it feels cold")
+  body.style.backgroundColor = "lightblue";
+} else if (temperature >= 20 && temperature < 30) {
+  console.log("it fees warm and sunny")
+  body.style.backgroundColor = "orange";
+} else if (temperature < 10) {
+  console.log("it is freezing")
+  body.style.backgroundColor = "white";
+} else if (temperature > 30) {
+  console.log("it is boiling")
+  body.style.backgroundColor = "red";
+}
+}
+
